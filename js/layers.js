6159ab3aaddc0554e429c0fafcfcfea7b3a03352
quwps <nicste28@bergen.org>
2025-03-16 20:36:43 -0400

    f

--- js/layers.js
+++ js/layers.js
@@ -8,8 +8,8 @@ addLayer("p", {
     }},
     color: "#FFDB77",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "prestige points", // Name of prestige currency
-    baseResource: "points", // Name of resource prestige is based on
+    resource: "Tincondergas", // Name of prestige currency
+    baseResource: "Floor Pencils", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
