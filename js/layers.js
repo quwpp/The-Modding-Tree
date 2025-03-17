0bba659b5ec772b7e7493dc008d19f1cb56d0902
quwps <nicste28@bergen.org>
2025-03-16 20:42:13 -0400

    f

--- js/layers.js
+++ js/layers.js
@@ -8,7 +8,7 @@ addLayer("p", {
     }},
     color: "#FFDB77",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "Tincondergas", // Name of prestige currency
+    resource: "Ticonderogas", // Name of prestige currency
     baseResource: "Floor Pencils", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
@@ -27,10 +27,6 @@ addLayer("p", {
     layerShown(){return true}, } )
     upgrades: {
         11; {
-            title: "Make this whatever you want!",
-            description;"Double your point gain.",
-            cost; new Decimal(1),
-            (hasUpgrade('p', 11)) ;gain = gain.plus(1)
-
+          
         }
     }
